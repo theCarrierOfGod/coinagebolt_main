5130ca8f724878760845a3501c2ef91181dfacbd
olaolumide <olajesujuwon@gmail.com>
2023-06-26 21:17:40 +0100

    complete project

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content>
+    <meta name="author" content>
+    <link rel="icon" href="/user/favicon.ico">
+
+    <title>Coinagebolt - Login </title>
+
+    <!-- Vendors Style-->
+    <link rel="stylesheet" href="/user/css/vendors_css.css">
+
+    <!-- Style-->
+    <link rel="stylesheet" href="/user/css/style.css">
+    <link rel="stylesheet" href="/user/css/skin_color.css">
+
+</head>
+
+<body class="hold-transition theme-primary bg-img" style="background-image: url(images/bg-2.jpg)">
+
+    <div class="container h-p100">
+        <div class="row align-items-center justify-content-md-center h-p100">
+
+            <div class="col-12">
+                <div class="row justify-content-center g-0">
+                    <div class="col-lg-5 col-md-5 col-12">
+                        <div class="bg-white rounded10 shadow-lg">
+                            <div class="content-top-agile p-20 pb-0">
+                                <h2 class="text-primary">Get started with Us</h2>
+                                <p class="mb-0">Register a new membership</p>
+                            </div>
+                            <div class="p-40">
+                                <form action="index.html" method="post">
+                                    <div class="form-group">
+                                        <div class="input-group mb-3">
+                                            <span class="input-group-text bg-transparent"><i class="ti-user"></i></span>
+                                            <input required type="text" class="form-control ps-15 bg-transparent" placeholder="Username">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group mb-3">
+                                            <span class="input-group-text bg-transparent"><i class="ti-lock"></i></span>
+                                            <input required type="password" class="form-control ps-15 bg-transparent" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <!-- /.col -->
+                                        <div class="col-12 text-center">
+                                            <button type="submit" class="btn btn-info margin-top-10">Register</button>
+                                        </div>
+                                        <!-- /.col -->
+                                    </div>
+                                </form>
+                                <div class="text-center">
+                                    <p class="mt-15 mb-0">Need an account?<a href="/register.php" class="text-danger ms-5"> Sign Up</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Vendor JS -->
+    <script src="js/vendors.min.js"></script>
+    <script src="js/chat-popup.js"></script>
+    <script src="js/feather.min.js"></script>
+
+
+</body>
+
+</html>
